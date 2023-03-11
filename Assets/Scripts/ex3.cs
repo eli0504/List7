c6928df251bd11305d0ac378f697b0a668f1e311
eli0504 <2003ecc@gmail.com>
2023-03-11 21:36:57 +0100

    ejercicio4

--- Assets/Scripts/ex3.cs
+++ Assets/Scripts/ex3.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ex3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int value = 0;
+    public TextMeshProUGUI valueText;
+    public int newPoints;
+
     void Start()
     {
-        
+        //text on creen
+        valueText.text = $"value: {value}";
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //Detect when the up arrow key is pressed up
+        {
+            value++; //sum of points
+            valueText.text = $"value: {value}";
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) // //Detect when the up arrow key is pressed down
+        {
+            value--; //points substraction
+            valueText.text = $"value: {value}";
+        }
     }
 }
--- Assets/Scripts/ex4.cs
+++ Assets/Scripts/ex4.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ex4 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject cubePrefab;
+
+    private void Start()
     {
-        
+        StartCoroutine("Position");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        transform.position = transform.position + new Vector3(2,1,0);
+
     }
+  
+    public IEnumerator Position()
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            yield return new WaitForSeconds(2);
+          
+            Vector3 pos = new Vector3(2,1,0);
+            GameObject inst = Instantiate(cubePrefab, pos, Quaternion.identity);
+        }
+    }
+
 }
